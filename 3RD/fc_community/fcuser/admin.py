f9e35aeef85c0f9b89612071e620924a8dc7aa64
hijoohee123 <hijoohee123@gmail.com>
2020-01-24 16:25:19 +0900

    django_project1

---
+++ 3RD/fc_community/fcuser/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import Fcuser
+
+class FcuserAdmin(admin.ModelAdmin):
+    list_display = (
+        'username','password'
+    )
+
+admin.site.register(Fcuser, FcuserAdmin)
+# Register your models here.
