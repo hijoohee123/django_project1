414e1226629d7c1d871dac99381034ace39969a8
hijoohee123 <hijoohee123@gmail.com>
2020-01-25 16:38:42 +0900

    login

--- 3RD/fc_community/fc_community/urls.py
+++ 3RD/fc_community/fc_community/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('fcuser/', include('fcuser.urls'))
+    path('fcuser/', include('fcuser.urls')),
+    path('', home)
 ]
 
